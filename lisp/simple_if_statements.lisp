4fb12c5724e8134d7e25c169f93dd911afff5b50
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-02-17 17:06:53 -0500

    Lisp if
    (if (test) (action if t) (action if nil)

---
+++ lisp/simple_if_statements.lisp
@@ -0,0 +1,27 @@
+#! /usr/bin/clisp
+
+
+(defvar a (read))
+
+(if (= a 15) (format t "~% a is 15") (format t "~% a is not 15"))
+
+(if (= a 20) (format t "~% A is 20") (format t "~% a is not 20"))
+
+(if (= a 30) (format t "~% a is 30") (format t "~% a is not 30"))
+
+(if (= a 40) (format t "~% a is 40") (format t "~% a is not 40"))
+
+(if (= a 50) (format t "~% a is 50") (format t "~% a is not 50"))
+
+(if (= a 60) (format t "~% a is 60") (format t "~% a is not 60"))
+
+(if (< a 100) (format t "~% a < 100") (format t "~% a > 100"))
+
+(if (< a 1000) (format t "~% a < 1000") (format t "~% a > 1000"))
+
+
+(format t "~% a = ~d" a)
+
+
+
+
