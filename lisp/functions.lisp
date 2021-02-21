5915640de935e233448f275c2228a55475092151
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-02-21 15:04:04 -0500

    Create functions.lisp

---
+++ lisp/functions.lisp
@@ -0,0 +1,15 @@
+#! /usr/bin/clisp
+
+
+(defun example_function(parameter) "Explanation of what this function does"
+  (write-line "This is the body of the function")
+  (print parameter)
+  (write-line "functions in lisp return the last statement of a function"))
+
+
+
+
+
+
+
+
