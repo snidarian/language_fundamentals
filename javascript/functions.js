7cd4804bfaa2206d2bb8da094c37323c595cf36c
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-03-02 11:42:12 -0500

    Update functions.js

--- javascript/functions.js
+++ javascript/functions.js
@@ -15,4 +15,6 @@ var second = prompt("Enter a second number: ")
 
 
 console.log("The product of " + first + " and " + second + " is " + exampleFunction(first, second))
+// or in a browser
+document.write("The product of " + first + " and " + second + " is " + exampleFunction(first, second))
 
