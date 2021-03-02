55606a09ad315e38809886b9730e137e116b7b42
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-03-02 11:39:53 -0500

    Add files via upload
    illustrates most basic use of functions in JavaScript

---
+++ javascript/functions.js
@@ -0,0 +1,18 @@
+// Illustration of the creating and use of functions
+
+
+function exampleFunction(value1, value2)
+{
+    var result = value1 * value2;
+
+    return result;
+}
+
+
+var first = prompt("Enter a number: ")
+
+var second = prompt("Enter a second number: ")
+
+
+console.log("The product of " + first + " and " + second + " is " + exampleFunction(first, second))
+
