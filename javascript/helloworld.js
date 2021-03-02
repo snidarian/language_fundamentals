6bd5167a7f18b7f2d16c76f68df35836a8a9f8d4
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-03-02 11:45:35 -0500

    Update helloworld.js

--- javascript/helloworld.js
+++ javascript/helloworld.js
@@ -1,11 +1,8 @@
 // Hello world program in javascript.
 
-
-
 console.log("Hello, World.");
-
-// If you want the text to output to browser window
-//document.write("Hello world")
+// or in a browser
+document.write("Hello, world.")
 
 
 
