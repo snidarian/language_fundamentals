bbcd97e140063efe7ee4faf55ec7e210511df08b
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-03-06 17:23:37 -0500

    Create helloworld.cpp

---
+++ c++/helloworld.cpp
@@ -0,0 +1,22 @@
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> msg {"Hello", "World"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+}
+
+
+
+
