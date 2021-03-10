1c8367df2cd984a4fe240a516199f66342cff585
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-03-10 06:53:06 -0500

    Create bow_and_arrow.sh

---
+++ bash/animation/bow_and_arrow.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+
+sleeper()
+{
+    sleep 0.05
+}
+
+while [ 1 ]; do
+    echo -ne "-)->                       |\r"
+    sleeper
+    echo -ne " )-->                      |\r"
+    sleeper
+    echo -ne " ) -->                     |\r"
+    sleeper
+    echo -ne " )  -->                    |\r"
+    sleeper
+    echo -ne " )   -->                   |\r"
+    sleeper
+    echo -ne " )    -->                  |\r"
+    sleeper
+    echo -ne " )     -->                 |\r"
+    sleeper
+    echo -ne " )      -->                |\r"
+    sleeper
+    echo -ne " )       -->               |\r"
+    sleeper
+    echo -ne " )        -->              |\r"
+    sleeper
+    echo -ne " )         -->             |\r"
+    sleeper
+    echo -ne " )          -->            |\r"
+    sleeper
+    echo -ne " )           -->           |\r"
+    sleeper
+    echo -ne " )            -->          |\r"
+    sleeper
+    echo -ne " )             -->         |\r"
+    sleeper
+    echo -ne " )              -->        |\r"
+    sleeper
+    echo -ne " )               -->       |\r"
+    sleeper
+    echo -ne " )                -->      |\r"
+    sleeper
+    echo -ne " )                 -->     |\r"
+    sleeper
+    echo -ne " )                  -->    |\r"
+    sleeper
+    echo -ne " )                   -->   |\r"
+    sleeper
+    echo -ne " )                    -->  |\r"
+    sleeper
+    echo -ne " )                     --> |\r"
+    sleeper
+    echo -ne " )                      -->|\r"
+    sleeper
+    echo -ne " )                      --|>\r"
+    sleeper
+    echo -ne " )                      --|>\r"
+    sleeper
+    
+    
+done
+
