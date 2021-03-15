e3206c4c9cf5be3f28f7e8e32c409b19eda7a2b8
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-03-15 17:55:10 -0400

    Create HelloWorld.java

---
+++ Java/HelloWorld.java
@@ -0,0 +1,12 @@
+
+
+// Java Hello world program                                                    
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+    }
+}
+
+
+
