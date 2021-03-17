052ae3aad9cc4dd0617f7974f55a674841dd2235
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-03-17 13:52:28 -0400

    Add files via upload

---
+++ Java/variables.java
@@ -0,0 +1,51 @@
+package scratch;
+
+public class variables {
+
+    /*
+    The general rules for constructing names for variables (unique identifiers) are:
+
+    Names can contain letters, digits, underscores, and dollar signs
+    Names must begin with a letter
+    Names should start with a lowercase letter and it cannot contain whitespace
+    Names can also begin with $ and _ (but we will not use it in this tutorial)
+    Names are case sensitive ("myVar" and "myvar" are different variables)
+    Reserved words (like Java keywords, such as int or boolean) cannot be used as names
+    */
+    
+    public static void main(String[] args) {
+        
+        // primitive data type examples
+        int a = 0;
+        String b = "name";
+        // Just like golang intellisense engine - java intellisense throws lint errors for unused variables.
+        int c = 45;
+        // finale keyword creates immutable variable (read only)
+        final int d = 55;
+        // notice the f after the float number.
+        float e = 14.99f;
+        // char just like in C
+        char f = 'K';
+        boolean g = true;
+        boolean h = false;
+
+
+        //many variables can be declared in a single line
+        int x = 5, y = 4, z = 3;
+        System.out.println(x + y + z);
+
+
+
+        System.out.println(g);
+        System.out.println(h);
+
+        System.out.println(e + c);
+        System.out.println(a + c + d);
+        System.out.println(b);
+        System.out.println(f);
+
+        // Abstract data type - object
+        
+    }
+
+}
