b5c15b2efbeac2f0194fccb88550ded8822b77d6
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-03-21 14:46:38 -0400

    Update type_of_builtin.lisp

--- lisp/type_of_builtin.lisp
+++ lisp/type_of_builtin.lisp
@@ -1,4 +1,5 @@
 #!/usr/bin/clisp
+;;;; type-of builtin function allows you to return the data type of a given variable (or struct if you used defstruct)
 
 ;; string
 (defvar a "stringy McString")
@@ -11,23 +12,24 @@
 ;; NULL - basically a symbolic constant for false or nothing
 (defvar e nil)
 
-
 (print (type-of a))
-
 (print (type-of b))
-
 (print (type-of c))
-
 (print (type-of d))
-
 (print (type-of e))
 
+(setq x 10)
+(setq y 34842398.28)
+(setq ch nil)
+(setq n 123.59)
+(setq bg 11.0e+4)
+(setq r 124/2)
 
-
-
-
-
-
-
+(print (type-of x))
+(print (type-of y))
+(print (type-of ch))
+(print (type-of n))
+(print (type-of bg))
+(print (type-of r))
 
 
