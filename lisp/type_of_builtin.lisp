c8d703e9603839db0fdc3efd885f6fc8650b914b
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-03-21 14:36:28 -0400

    Add files via upload

---
+++ lisp/type_of_builtin.lisp
@@ -0,0 +1,33 @@
+#!/usr/bin/clisp
+
+;; string
+(defvar a "stringy McString")
+;; single float
+(defvar b 45.25)
+;; integer
+(defvar c 72)
+;; symbol
+(defvar d 'symbol)
+;; NULL - basically a symbolic constant for false or nothing
+(defvar e nil)
+
+
+(print (type-of a))
+
+(print (type-of b))
+
+(print (type-of c))
+
+(print (type-of d))
+
+(print (type-of e))
+
+
+
+
+
+
+
+
+
+
