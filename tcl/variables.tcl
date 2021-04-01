4d0a3ff6025e76283323497242c494930df05382
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-04-01 15:47:43 -0400

    Add files via upload

---
+++ tcl/variables.tcl
@@ -0,0 +1,31 @@
+#!/usr/bin/tclsh
+
+
+
+# variable typing is dynamic and automatic
+# setting a variable
+
+set variable_name 10
+puts $variable_name
+
+
+# As "variable_name" exists right now, it is a string
+# but when we use it to make an arithmetic expression
+# the value "10" as a string is automaticall converted
+# to an integer by the interpreter.
+
+puts [expr $variable_name + 147 - 72]
+
+
+# In tcl variables are given no starting value and must
+# be initialized by giving them a value before they are used.
+
+
+
+# Set a variable to the value of a string with "" or {}
+set double_quotes_var "string"
+set double_braces_var {string}
+
+puts "$double_braces_var , $double_quotes_var"
+
+
