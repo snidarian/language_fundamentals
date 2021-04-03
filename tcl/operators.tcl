5f70d83f4d10b784af7a26cc0a61df2a7e419be8
snidarian <cephalopod31956@gmail.com>
2021-04-03 15:11:59 -0400

    file preliminary

---
+++ tcl/operators.tcl
@@ -0,0 +1,79 @@
+#!/usr/bin/tclsh
+
+# OPERATORS in tcl
+
+
+# ARITHMETIC OPERATORS
+
+set a 15
+set b 20
+
+# ADDITION
+puts [expr $a + $b]
+# SUBTRACTION
+puts [expr $a - $b]
+# MULTIPLICATION
+puts [expr $a * $b]
+# DIVISION
+puts [expr $a / $b]
+# MODULUS/REMAINDER
+puts [expr $a % $b]
+
+
+# RELATIONAL OPERATORS
+
+# EQUAL TO
+
+# '=='
+
+# NOT EQUAL
+
+# '!='
+
+# GREATER THAN
+
+# '>'
+
+# LESS THAN
+
+# '<'
+
+# GREATER OR EQUAL
+
+# >=
+
+# LESSER OR EQUAL
+
+# <=
+
+
+# LOGICAL OPERATORS
+
+# AND
+
+# &&
+
+# OR
+
+# ||
+
+# NOT
+
+# !
+
+
+# TERNARY OPERATORS
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
