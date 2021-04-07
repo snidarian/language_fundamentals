bef048917ab8a6afd1cdd372bfda3f3983566e3f
snidarian <cephalopod31956@gmail.com>
2021-04-07 09:56:12 -0400

    special variables doc file

---
+++ tcl/tk/tk_special_variables.tcl
@@ -0,0 +1,10 @@
+#!/usr/bin/env wish
+
+# $tk_library - used for setting location of tk libraries
+# $tk_patchLevel - current patch level of the tk interpreter
+# $tk_strictMotif - when non-zero value tk trie to adhere to motif look and feel
+# $tk_version - displays the tk version
+
+
+puts "$tk_library , $tk_patchLevel , $tk_strictMotif , $tk_version"
+
