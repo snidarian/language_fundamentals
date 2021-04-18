9b0b2c5846df593900c8053164087c2210fbd706
snidarian <cephalopod31956@gmail.com>
2021-04-18 09:04:52 -0400

    exec doc-file's examples expanded

--- tcl/exec.tcl
+++ tcl/exec.tcl
@@ -1,20 +1,47 @@
 #!/usr/bin/tclsh
 
 # exec command spawns a subprocess.
+# This doc-file exhibits some of the uses of the exec command.
+
+# a simple way to use the exec command is to use it in a command substitution and
+# and assign a variable (using set) to the result of the command substitution.
 
 
 # set a variable to the command substitution of exec on a linux command
-set returnvar [exec ls]
+set lsvar [exec ls]
 
 # The normal output of ls is added to this command
-puts $returnvar
+puts $lsvar
+
+
+# The output of cat used on a file in the current directory
+set catvar [exec cat file.txt]
+
+# print the output
+puts $catvar
 
 
 
+# Create a file in the current directory with the touch command
+exec touch example_file.txt
 
 
+# removes the file just created form the current directory 
+exec rm example_file.txt
 
 
+# cd is a builtin command in tcl!! You can change directories with the script.
+cd ~
+
+set lshome [exec ls]
+
+puts $lshome
+
+
+# list the contents of the root directory
+set lsroot [exec ls /]
+
+puts $lsroot
 
 
 
