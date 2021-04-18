3526bb756d2c78a67606eddca2b0a635803b6d86
snidarian <cephalopod31956@gmail.com>
2021-04-18 09:31:36 -0400

    expanded namespaces.tcl

--- tcl/namespaces.tcl
+++ tcl/namespaces.tcl
@@ -24,6 +24,27 @@ puts $::MyMath::myResult
 
 
 
+# Nested namespaces
+# tcl allows the nesting of namespaces.
+
+namespace eval MyMath {
+   # Create a variable inside the namespace
+   variable myResult
+}
+
+namespace eval extendedMath {
+   # Create a variable inside the namespace
+   namespace eval MyMath {
+      # Create a variable inside the namespace
+      variable myResult
+   }
+}
+set ::MyMath::myResult "test1"
+puts $::MyMath::myResult
+set ::extendedMath::MyMath::myResult "test2"
+puts $::extendedMath::MyMath::myResult
+
+
 
 
 
