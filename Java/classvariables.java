701d3a624f4661ff4c265490d9b66faabe538d09
snidarian <cephalopod31956@gmail.com>
2021-04-19 16:07:20 -0400

    class variables doc file

---
+++ Java/classvariables.java
@@ -0,0 +1,16 @@
+// Class variables are variables that can be used by the methods of a class
+// There two rules when declaring class variables:
+// 1. You must place the variable declaration within the body of the class
+// (but not within any of the class methods.)
+
+// 2. You must include the word 'static' in the declaration.
+// (the word static comes before the variable type)
+
+
+public class classvariables {    
+    static String helloMessage = "hello, world";
+    public static void main(String[] args) {
+        System.out.println(helloMessage);
+    }
+}
+
