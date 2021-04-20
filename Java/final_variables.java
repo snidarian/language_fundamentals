ae1135d0023c26b817bc8c15b9c128578845cf1b
snidarian <cephalopod31956@gmail.com>
2021-04-20 18:46:46 -0400

    word

--- Java/final_variables.java
+++ Java/final_variables.java
@@ -1,12 +1,15 @@
 public class final_variables {
     
+    // This is a static constant class-variable
+    static final int MINS_IN_HOUR = 60;
+
+
     public static void main(String[] args) {
         final int WEEKDAYS = 5;
         final int WEEKEND_DAYS = 2;
 
         System.out.println("There are " + WEEKDAYS + " weekdays");
         System.out.println("There are " + WEEKEND_DAYS + " weekend days");
-
 
 
 
