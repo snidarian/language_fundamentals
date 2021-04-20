5ff9b0f07d4c6493c96ca07f9784bd092686b4ea
snidarian <cephalopod31956@gmail.com>
2021-04-20 12:15:09 -0400

    java doc-file

---
+++ Java/local_variables.java
@@ -0,0 +1,21 @@
+// a local variable is a variable that is declared within the body of a method
+
+// Local variable do not need to be declared static or otherwise
+// since they only exist during the execution of that method their
+// whether or not they're in an instance of a class is irrelevant
+
+public class local_variables {
+    
+
+    public static void main(String[] args) {
+        String greeting;
+        greeting = "Hello. My name is Komp Yuter.";
+        System.out.println(greeting);
+    }
+
+
+
+}
+
+
+
