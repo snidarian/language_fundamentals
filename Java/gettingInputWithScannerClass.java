11a7609c5d51ec69d804f147b3bc674533b0d9c4
snidarian <cephalopod31956@gmail.com>
2021-04-26 15:27:58 -0400

    addition

--- Java/gettingInputWithScannerClass.java
+++ Java/gettingInputWithScannerClass.java
@@ -4,11 +4,17 @@ import java.util.Scanner;
 
 public class gettingInputWithScannerClass {
     // creating a scanner object from Scanner class to use in program
+    // To create a Scanner object, you use the 'new' keyword followed by a call to the Scanner class constructor
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args)
     {
         System.out.print("Enter and integer: ");
         int x = sc.nextInt(); // nextin() method converts input to primitive integer data type
         System.out.println("You entered " + x + ".");
+        
+        // now the same thing but with a string. Notice the use of of a different method this time
+        System.out.print("Enter a string: ");
+        String str = sc.next();
+        System.out.println(str);
     }
 }
