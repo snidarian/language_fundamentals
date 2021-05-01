9dd131475b810b2bc039d914a0047a06bcea02bb
snidarian <cephalopod31956@gmail.com>
2021-05-01 05:40:44 -0400

    assoc arrays in tcl

---
+++ tcl/associative_arrays.tcl
@@ -0,0 +1,36 @@
+#!/usr/bin/tclsh
+
+# associative arrays in tcl act more or less like key value pairs
+# in a python dictionary.
+
+
+# Creating associative arrays
+set fruits(red) "strawberry"
+# printing the value within an associateive arrays
+puts $fruits(red)
+
+set fruits(blue) "blueberry"
+
+puts $fruits(blue)
+
+set fruits(yellow) "banana"
+
+puts $fruits(yellow)
+
+set fruits(orange) "orange"
+
+puts $fruits(orange)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
