3502bd3955c60d38ee97d222536f2aa0ce3241ba
snidarian <cephalopod31956@gmail.com>
2021-05-01 20:51:43 -0400

    clarification

--- bash/dialog_command/yesno_box.sh
+++ bash/dialog_command/yesno_box.sh
@@ -4,15 +4,23 @@
 
 dialog --title "Confirmation" --yesno "Are you sure?" 10 40
 
+# variable set equal to exit status of dialog command
 var=$?
 
 echo "exit code = $var"
 
-# the exit code value is used to determined which option was selected by the user.
+
+# clear the gui after its usefulness elapses
+clear
+
+# the exit code value is used to determine which option was selected by the user.
 if [ $var -eq 0 ]; then
     echo "you said yes"
 else
     echo "you said no"
 fi
 
+# sleep to show the feedback was recieved and noted
+sleep 2
 
+clear
