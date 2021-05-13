bb1a7ad77172700282d49b2716e4d6bc7db61803
snidarian <cephalopod31956@gmail.com>
2021-05-13 12:53:28 -0400

    initial commit

---
+++ javascript/document_object_model/dom_manipulation.js
@@ -0,0 +1,27 @@
+// There are many ways to manipulate the Document Object model in JavaScript
+
+
+
+
+document.write("words")
+
+const body = document.body
+
+const div = document.createElement('div')
+
+
+div.innerHTML = "<p><strong>hello world</strong></p>"
+
+
+body.append(div)
+document.body.append(div)
+
+
+
+
+div.style.backgroundColor = "red"
+
+
+
+
+
