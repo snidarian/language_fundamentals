a49f4669527fcab1b413bb6703ea201106e64802
snidarian <cephalopod31956@gmail.com>
2021-05-25 17:22:32 -0400

    changes

---
+++ python3/asynchronous/async_requests.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+import asyncio
+import logging
+import re
+import sys
+import typing
+import io
+import urllib.error
+import urllib.parse
+
+import aiofiles
+import aiohttp
+
+
+
+
+
+
+
+
+
+
+
