e52bc092731d997874e3acf866a02fedc0365d78
snidarian <cephalopod31956@gmail.com>
2021-05-26 08:03:35 -0400

    init commit

--- python3/modules/wikipedia/wikisearch.py
+++ python3/modules/wikipedia/wikisearch.py
@@ -4,16 +4,30 @@
 
 import wikipedia
 import argparse
+from colorama import Fore, Style
 
 
+# Argparse takes care of the arguments passed to the program
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-s", help="Search term string")
+parser.add_argument("-s", "--search-term", help="Search term string", type=str, default="Wikipedia")
+
+args = parser.parse_args()
 
 
-result = wikipedia.page("Tibetan Book of the dead")
+# Checks whether of not you might have mispelled search term
+spellcheck = wikipedia.suggest(args.search_term) # returns None if no search suggestion is found
+if spellcheck != None:
+    print(f"Did you by chance mean '{spellcheck}'?")
+else:
+    pass
 
-print(result.summary)
+try:
+    result = wikipedia.page(args.search_term)
+    print(result.summary)
+except:
+    print("Some sort of error encountered")
+
 
 
 
