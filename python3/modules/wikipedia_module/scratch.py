01a7bc680a307c4f19852f888e99054a8aa2eeda
snidarian <cephalopod31956@gmail.com>
2021-05-26 20:41:14 -0400

    new feature

--- python3/modules/wikipedia_module/scratch.py
+++ python3/modules/wikipedia_module/scratch.py
@@ -3,16 +3,8 @@
 import wikipedia
 
 
-def return_random_pages(quantity):
-    random_results = wikipedia.random(pages=quantity)
-    print(random_results)
-    for term in random_results:
-        # retrieve page content summary
-        page = wikipedia.page(title=term)
-        uppercased_term = term.upper()
-        print("---------------------------------------------------------", end="\n")
-        print(str(uppercased_term), end="\n\n")
-        print(page.summary)
-        print("---------------------------------------------------------", end="\n\n")
+
+print(wikipedia.languages())
+
 
 
