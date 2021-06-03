854deb003a95f557c9e668328e33fe3a6091a607
snidarian <cephalopod31956@gmail.com>
2021-06-03 09:36:55 -0400

    many colored stars

---
+++ python3/modules/turtle_module/many_colored_stars.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import turtle
+import random as r
+import time
+
+pen = turtle.Turtle()
+pen.speed(7)
+color_list = ['red', 'blue', 'green', 'yellow', 'cyan', 'black', 'white', 'teal', 'maroon', 'pink']
+turtle.bgcolor(r.choice(color_list))
+
+def draw_star(x_value: int, y_value: int, scale: int, outline_color: str, fill_color: str) -> None:
+    pen.penup()
+    pen.setpos(x_value, y_value)
+    pen.pendown()
+    pen.color(outline_color, fill_color)
+    pen.begin_fill()
+    # dichotomize star heading
+    if (r.randint(0, 1)):
+        for _ in range(5):
+            pen.forward(scale * 100)
+            pen.left(144)
+    else:
+        for _ in range(5):
+            pen.forward(scale * 100)
+            pen.right(144)
+    pen.end_fill()
+
+
+while True:
+    randomx = r.randint(-500, 500)
+    randomy = r.randint(-300, 300)
+    random_outline_color = r.choice(color_list)
+    random_fill_color = r.choice(color_list)
+    random_scale = r.randint(1, 3)
+    draw_star(randomx, randomy, random_scale, random_outline_color, random_fill_color)
+    time.sleep(.5)
---
+++ python3/modules/turtle_module/yellow_flower.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# creates yellow flower like shape
+
+import turtle
+import random as r
+
+
+pen = turtle.Turtle()
+turtle.bgcolor('blue')
+pen.color('red', 'yellow')
+pen.speed(0)
+
+
+#pen.begin_fill()
+for _ in range(70):
+    offset = r.randint(0, 1)
+    pen.forward(300)
+    pen.left(150 + offset)
+#pen.end_fill()
+
+
+# we don't want to do away with the GUI after we're done drawing
+turtle.done()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
