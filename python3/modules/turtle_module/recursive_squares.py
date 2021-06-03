69bf2e23c5e25ef1d1926ed077d8611a3f9588a5
snidarian <cephalopod31956@gmail.com>
2021-06-03 19:50:23 -0400

    recursive rectangles

---
+++ python3/modules/turtle_module/recursive_squares.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import turtle
+
+pen = turtle.Turtle()
+pen.getscreen().bgcolor('green')
+pen.color('blue')
+pen.speed(0)
+
+
+def draw_square(size) -> None:
+    if size < 3:
+        return
+    else:
+        for _ in range(4):
+            pen.forward(size*2)
+            draw_square(size/2)
+            pen.right(90)
+
+
+
+draw_square(70)
+
+
+turtle.done()
+
+
+
+
+
