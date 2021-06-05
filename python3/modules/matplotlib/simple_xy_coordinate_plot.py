60e284fdadcd4ddb91fecd904baa8af95b21373e
snidarian <cephalopod31956@gmail.com>
2021-06-05 11:06:04 -0400

    mathplottinglib

--- python3/modules/matplotlib/simple_xy_coordinate_plot.py
+++ python3/modules/matplotlib/simple_xy_coordinate_plot.py
@@ -9,13 +9,14 @@ import numpy as np
 # variables lists that contain the values for the coordinates to be plotted
 x = [1, 2, 10, 15, 30, 20]
 y = [2, 4, 20, 30, 60, 40]
-
+z = [2 ,3, 5, 59, 110 ,100]
 
 # plots the xy coordinates
 # optional third argument to the plot() method is color and coordinate formatting. The default is 'b-' for a blue line. 
 # 'ro' renders red orbs and 'g^' renders green caret-marks. green squares: 'gs', red dashes: 'r--'
-plt.plot(x, 'r^', label='label0')
-plt.plot(y, 'gs', label='label1')
+plt.plot(x, 'r-', label='label0', linewidth=3, linestyle='--')
+plt.plot(y, 'gs', label='label1', linewidth=2, markersize=7, markeredgecolor='red')
+plt.plot(z, 'y--', label='label2', marker='^', markersize='15', markeredgecolor='b')
 
 
 # Gives a title to the graph
