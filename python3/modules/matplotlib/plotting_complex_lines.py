90711a63ab92651e8a99cf4e74e1880e820cc898
snidarian <cephalopod31956@gmail.com>
2021-06-05 14:27:50 -0400

    yes

--- python3/modules/matplotlib/plotting_complex_lines.py
+++ python3/modules/matplotlib/plotting_complex_lines.py
@@ -7,14 +7,15 @@ import numpy as np
 
 
 
-x = np.arange(0,30, 1)
+x = np.arange(0,10000, 1)
 
 print(x)
 
-# notice the squared expression below. Matplotlib will plot this. For every x list-value it will use the squared expression to plot it
-plt.plot(x*10000, 'g--', linewidth=2, label='Population growth')
-plt.plot(x, 'r--', linewidth=3, label='Number of sociopaths')
+# notice the expression below. Matplotlib will plot this. For every x list-value it will use calculate the expression to plot it
+plt.plot(x, 'g--', linewidth=2, label='Population')
+plt.plot(x*.03, 'r--', linewidth=3, label='Number of sociopaths')
 
+plt.title('3 Male (1 female) sociopaths in every 100 persons')
 
 plt.legend()
 
