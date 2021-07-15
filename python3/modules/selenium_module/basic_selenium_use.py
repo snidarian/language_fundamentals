f200762f20623e9550d676a260d8f5a5db57f6d6
snidarian <cephalopod31956@gmail.com>
2021-07-15 13:42:10 -0400

    basic usage scheme

---
+++ python3/modules/selenium_module/basic_selenium_use.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+from selenium import webdriver
+
+
+# Creating new instance of browser
+browser = webdriver.Firefox(executable_path='/usr/bin/firefox')
+
+
+
+# perform a get request on the server
+browser.get('https://www.linkedin.com/')
+
+
+
+
+
+
+
+
