9e5ddbe358a38072027c4195f6a5bde2d3392d2f
snidarian <cephalopod31956@gmail.com>
2021-09-18 23:23:50 -0400

    lakjsdlfkj

--- python3/modules/multiprocessing_module/multiprocessing_queue_basic_usage.py
+++ python3/modules/multiprocessing_module/multiprocessing_queue_basic_usage.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
+# Uses the multiprocessing module to calculate large quantity of sequential squared integer
 
 
 import multiprocessing
 import numpy
+import os
 
 
 def calculate_squares(numbers, q):
-    
     for n in numbers:
         q.put((n,n*n))
-        print(f'(Inside process): {n}^2 = ' + str(n*n))
+        print(f'PID {os.getpid()}: {n}^2 = ' + str(n*n))
 
 
 if __name__ == "__main__":
-    number_set = numpy.arange(1, 200, 1)
+    # .arrange() create an array of sequential integers
+    # .reshape() creates a 2d array of X lists of Z size
+    number_set = numpy.arange(1, 801, 1).reshape(8,100)
     # multiprocessing.Queue() class is used to shared data between processes
     q = multiprocessing.Queue()
-    #p = multiprocessing.Process(target=calculate_squares, args=(number_set, q))
-
-    print(number_set)
-
-
-    #p.start()
-
-    #p.join()
+    
+    for set_of_100 in number_set:
+        p = multiprocessing.Process(target=calculate_squares, args=(set_of_100, q))
+        p.start()
+        p.join()
 
     while q.empty() is False:
         print(q.get())
